9a78529d10af52782944cb2cf7685f2256aa0d1f
Ubinquitous <ubinquitous1@gmail.com>
2022-09-01 13:01:12 +0900

    ADD : 프로필사진, 유저명, 날짜, 내용 요소 추가

--- src/Components/ForumItem.js
+++ src/Components/ForumItem.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import '../Styles/ForumItem.css';
 
 const ForumItem = ({ key, text }) => {
     return (
-        <div className='ㅅ'>
-            do
+        <div className='forumItem'>
+            <div className='forumItemBox'>
+                <img src={require('../images/defaultProfile.jpeg')} className='profile' />
+                <div className='username'>{'Ubin'}</div><br /><br />
+                <div className='date'>{'2022-08-31'}</div><br />
+                <div className='content'>{'asnjbkkvnjvnjknkankvjn'}</div>
+            </div>
         </div>
     );
 };
