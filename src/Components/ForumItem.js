756cc6e94a5fb67ded15baa138ba4da7cab7fd5d
Ubinquitous <ubinquitous1@gmail.com>
2022-09-01 20:26:10 +0900

    FIX, UPDATE : 이미지 파일 경로 변경, 조건부로 props 전달

--- src/Components/ForumItem.js
+++ src/Components/ForumItem.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../Styles/ForumItem.css';
 
-const ForumItem = ({ key, text }) => {
+const ForumItem = ({ text, name, date, profileImg, contentImg }) => {
+
+    console.log(contentImg);
+
     return (
         <div className='forumItem'>
             <div className='forumItemBox'>
-                <img src={require('../images/defaultProfile.jpeg')} className='profile' />
-                <div className='username'>{'Ubin'}</div><br /><br />
-                <div className='date'>{'2022-08-31'}</div><br />
-                <div className='content'>{'asnjbkkvnjvnjknkankvjn'}</div>
+                <img src={profileImg} className='profile' />
+                <div className='username'>{name}</div><br /><br />
+                <div className='date'>{date}</div><br />
+                <div className='line' />
+                {contentImg !== 'undefined' ? (
+                    <img src={contentImg} className='articleImg' />
+                ) : null}
+
+                <div className='content'>{text}</div>
             </div>
         </div>
     );
