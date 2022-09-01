acf1aba92f918dbc82f0ade70df1d44d91244bed
Ubinquitous <ubinquitous1@gmail.com>
2022-09-02 01:38:04 +0900

    ADD : 다크모드, 기본모드 헤더바 기능 생성

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Styles/Header.css';
 import '../Styles/Header_Section.css'
 
 const Header = () => {
+    const [images, setImages] = useState('Sun');
+    const [logo, setLogo] = useState('T-Logo');
+    const [dark, setDark] = useState(false);
+
+    const onClick = () => {
+        if (images === 'Sun') {
+            setImages('Moon');
+            setLogo('T-Logo-White');
+        } else if (images === 'Moon') {
+            setImages('Sun');
+            setLogo('T-Logo');
+        }
+    }
+
     return (
         <div>
-            <div className='headerBox'>
-                <img src='../images/T-Logo.png' className='headerLogo' />
+            <div className='headerBox'
+                style={
+                    images === 'Sun' ? {
+                        backgroundColor: 'white'
+                    } : {
+                        backgroundColor: 'black'
+                    }
+                }>
+                <img src={`/images/${logo}.png`} className='headerLogo' alt='logo' />
+                <img src={`/images/${images}.png`} className='changeModeBtn' alt='changeModeButton' onClick={onClick} />
             </div>
+
             <div className='section'>
                 <div className='home-section'>
                     HOME
