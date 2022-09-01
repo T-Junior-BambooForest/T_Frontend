34c4cd9ebb10b7bcc6291bb2888c82b1b792b2d0
Ubinquitous <ubinquitous1@gmail.com>
2022-09-01 20:26:45 +0900

    FIX : 로고 이미지 변경

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import '../Styles/Header.css';
+import '../Styles/Header_Section.css'
 
 const Header = () => {
     return (
-        <div className='headerBox'>
-            <img src={require('../images/T-Logo.png')} className='headerLogo' />
+        <div>
+            <div className='headerBox'>
+                <img src='../images/T-Logo.png' className='headerLogo' />
+            </div>
+            <div className='section'>
+                <div className='home-section'>
+                    HOME
+                </div>
+                <div className='forum-section'>
+                    FORUM
+                </div>
+                <div className='more-section'>
+                    MORE
+                </div>
+                <div className='option-section'>
+                    OPTION
+                </div>
+            </div>
         </div>
     );
 };
