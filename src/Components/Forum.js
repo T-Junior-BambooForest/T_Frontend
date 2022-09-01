845f12ecbae6d84647031040645aaf9c0d75a2c2
Ubinquitous <ubinquitous1@gmail.com>
2022-09-01 20:25:30 +0900

    FIX : 이미지 파일 경로 변경

--- src/Components/Forum.js
+++ src/Components/Forum.js
@@ -1,46 +1,44 @@
 import React, { useState } from 'react';
 import ForumItem from './ForumItem.js';
+import defaultProps from 'prop-types';
 
 const Forum = () => {
     const [items, setItems] = useState(
         [
-            // {
-            //     id: 0,
-            //     text: 'Hello, My name is Park Ubin. I am frontend Developer.',
-            //     name: 'Park Ubin',
-            //     date: '2022-09-01',
-            // },
-            // {
-            //     id: 1,
-            //     text: "It is just Text content. so this sentence is don't have mean. Have a nice day, Bro.",
-            //     name: 'Test Case Developer',
-            //     date: '2021-12-28',
-            // },
-            // {
-            //     id: 2,
-            //     text: "hihi",
-            //     name: '박우빈',
-            //     date: '2022-01-31',
-            // },
-            // {
-            //     id: 3,
-            //     text: "Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter ",
-            //     name: 'Hello World',
-            //     date: '2006-03-27',
-            // },
             {
-                id: 4,
+                id: 0,
+                text: 'Hello, My name is Park Ubin. I am frontend Developer.',
+                name: 'Park Ubin',
+                date: '2022-09-01',
+                profileImg: '/images/picture.png',
+            },
+            {
+                id: 1,
+                text: "hihi",
+                name: '박우빈',
+                date: '2022-01-31',
+                profileImg: '/images/ForumTitleBlack.png',
+            },
+            {
+                id: 2,
+                text: "Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter Just Longer letter ",
+                name: 'Hello World',
+                date: '2006-03-27',
+                profileImg: '/images/undefined.jpeg',
+            },
+            {
+                id: 3,
                 text: 'please',
                 name: 'Image Maker',
                 date: '2022-08-13',
-                profileImg: '../images/undefined.jpeg',
-                contentImg: '../images/picture.png',
+                contentImg: '/images/picture.png',
+                profileImg: '/images/undefined.jpeg',
             }
         ]
     );
     return (
         <div>
-            <img src={require('../images/ForumTitleWhite.png')} className='forumTitle' />
+            <img src='/images/ForumTitleWhite.png' className='forumTitle' />
             {
                 items.map((item) => (
                     <ForumItem
@@ -49,11 +47,13 @@ const Forum = () => {
                         name={item.name}
                         date={item.date}
                         profileImg={item.profileImg}
+                        contentImg={item.contentImg}
                     />
                 ))
             }
         </div>
     );
 };
+
 
 export default Forum;
