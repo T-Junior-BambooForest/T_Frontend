5801bca8c5ee4235ba614b2051a4911c438db1ea
Ubinquitous <ubinquitous1@gmail.com>
2022-09-05 00:31:11 +0900

    ADD : 다크모드 적용

--- src/Components/ForumItem.js
+++ src/Components/ForumItem.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import '../Styles/ForumItem.css';
 
-const ForumItem = ({ text, name, date, profileImg, contentImg }) => {
+const ForumItem = ({ text, name, date, profileImg, contentImg, images }) => {
+
 
     return (
-        <div className='forumItem'>
-            <div className='forumItemBox'>
-                <img src={profileImg} className='profile' alt='profile' />
-                <div className='username'>{name}</div><br /><br />
-                <div className='date'>{date}</div><br />
-                <div className='line' />
-                {contentImg !== 'undefined' ? (
-                    <img src={contentImg} className='articleImg' />
-                ) : null}
+        <div>
+            {images === 'Sun' ?
+                <div className='forumItem'>
+                    <div className='forumItemBox'>
+                        <img src={profileImg} className='profile' alt='profile' />
+                        <div className='username'>{name}</div><br /><br />
+                        <div className='date'>{date}</div><br />
+                        <div className='line' />
+                        {contentImg !== 'undefined' ? (
+                            <img src={contentImg} className='articleImg' />
+                        ) : null}
 
-                <div className='content'>{text}</div>
-            </div>
+                        <div className='content'>{text}</div>
+                    </div>
+                </div>
+                :
+                <div className='forumItemDark'>
+                    <div className='forumItemDarkBox'>
+                        <img src={profileImg} className='profile' alt='profile' />
+                        <div className='usernamedark'>{name}</div><br /><br />
+                        <div className='datedark'>{date}</div><br />
+                        <div className='line' />
+                        {contentImg !== 'undefined' ? (
+                            <img src={contentImg} className='articleImg' />
+                        ) : null}
+
+                        <div className='contentdark'>{text}</div>
+                    </div>
+                </div>}
         </div>
     );
 };
