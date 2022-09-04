01614ff76aebaa7ad8cc46a80889ce2ebaae5ea5
Ubinquitous <ubinquitous1@gmail.com>
2022-09-05 00:33:24 +0900

    ADD, FIX : 다크모드 버튼위치 Header에서 App으로 변경, props 전달

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Forum from './Components/Forum';
 import Header from './Components/Header';
+import './Styles/Header.css';
 
 const App = () => {
+  const [images, setImages] = useState('Sun');
+  const [logo, setLogo] = useState('T-Logo');
+
+  const onClick = () => {
+    if (images === 'Sun') {
+      setImages('Moon');
+      setLogo('T-Logo-White');
+    } else if (images === 'Moon') {
+      setImages('Sun');
+      setLogo('T-Logo');
+    }
+  }
   return (
-    <div>
-      <Header />
-      <Forum />
+    <div className='how'
+      style={
+        images === 'Sun' ? {
+          backgroundColor: 'white',
+        } : {
+          backgroundColor: 'black'
+        }
+      }>
+
+
+      <div className='headerBox'
+        style={
+          images === 'Sun' ? {
+            backgroundColor: 'white'
+          } : {
+            backgroundColor: '#101010'
+          }
+        }>
+        <img src={`/images/${logo}.png`} className='headerLogo' alt='logo' />
+        <img src={`/images/${images}.png`} className='changeModeBtn' alt='changeModeButton' onClick={onClick} />
+      </div>
+      <Header images={images} />
+      <Forum images={images} />
     </div>
   );
 };
