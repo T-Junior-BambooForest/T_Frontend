7ea8bbb3eff752efb9487a27a9cc8295250323f4
Ubinquitous <ubinquitous1@gmail.com>
2022-09-16 00:55:34 +0900

    ADD : Header, Post 컴포넌트 렌더링

---
+++ src/Components/Router/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header';
+import Post from '../Post';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Post />
+        </div>
+    );
+};
+
+export default Home;
