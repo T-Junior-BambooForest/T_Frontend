d7b450d0d03c9af83a0bc60548d635fa83047fbb
Ubinquitous <ubinquitous1@gmail.com>
2022-09-18 02:40:31 +0900

    UPDATE : Header 컴포넌트 재구성

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../Style/Header.scss';
 
-const Header = () => {
+const Header = ({ isLogin }) => {
+
     return (
         <div className='header_wrap'>
             <img src='/images/logoBlack.png' alt='로고' className='logo_image'></img>
-            <h1 className='title'>BSMBOO</h1>
+            <Link to='/' className='title_link_tag'><h1 className='title'>BSMBOO</h1></Link>
             <h1 className='subtitle'>부산소프트웨어마이스터고 대나무숲</h1>
             <div className='login_wrap'>
-                <span className='mypage_button'>마이페이지</span>
+                {isLogin ?
+                    <Link to='/mypage' className='mypage_button'>마이페이지</Link>
+                    :
+                    <Link to='/login' className='mypage_button'>마이페이지</Link>
+                }
                 <div className='custom_line' />
-                <span className='login_button'>로그인</span>
+                <a className='login_button' href='https://bssm.kro.kr/oauth/login?clientId=59b9bb6b&redirectURI=http://bsmboo.kro.kr/oauth'>로그인</a>
             </div>
         </div>
     );
