76e03b7aeefc633440b3128723df7dbd36a2f4d1
Ubinquitous <ubinquitous1@gmail.com>
2022-09-18 21:51:20 +0900

    FIX : 에디터 박스 길이 vw로 변경

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -13,33 +13,35 @@ const Post = () => {
 
     return (
         <div className='article_wrap'>
-            <div className='post_title_box'>
-                <h1 className='post_title'>글 작성하기</h1>
-            </div>
-            <div className='editor_box'>
-                <Editor
-                    className='editor'
-                    apiKey='iple01nb1njvg519n1xm9kw2ok1gfv0u5jyx4ety2gc2ay16'
-                    onInit={(evt, editor) => editorRef.current = editor}
-                    init={{
-                        language: 'ko_KR',
-                        skin: undefined,
-                        placeholder: '여기에 글을 작성해주세요!',
-                        height: 200,
-                        width: 900,
-                        menubar: true,
-                        content_css: 'body { margin: 0 auto; }',
-                        plugins: [
-                            'code', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'searchreplace', 'visualblocks', 'table', 'wordcount', 'codesample'
-                        ],
-                        toolbar: 'undo redo codesample | bold italic | alignleft alignright aligncenter | emoticon image media ',
+            <div className='article_box'>
+                <div className='post_title_box'>
+                    <h1 className='post_title'>글 작성하기</h1>
+                </div>
+                <div className='editor_box'>
+                    <Editor
+                        className='editor'
+                        apiKey='iple01nb1njvg519n1xm9kw2ok1gfv0u5jyx4ety2gc2ay16'
+                        onInit={(evt, editor) => editorRef.current = editor}
+                        init={{
+                            language: 'ko_KR',
+                            skin: undefined,
+                            placeholder: '여기에 글을 작성해주세요!',
+                            height: 200,
+                            width: '65vw',
+                            menubar: true,
+                            content_css: 'body { margin: 0 auto; }',
+                            plugins: [
+                                'code', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'searchreplace', 'visualblocks', 'table', 'wordcount', 'codesample'
+                            ],
+                            toolbar: 'undo redo codesample | bold italic | alignleft alignright aligncenter | emoticon image media ',
 
-                        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px; }',
-                    }}
+                            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px; }',
+                        }}
 
-                />
+                    />
+                </div>
+                <Forum />
             </div>
-            <Forum />
         </div>
 
     );
