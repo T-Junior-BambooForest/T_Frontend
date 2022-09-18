127493f4af2ef511d1fc542fd580c0f12f65eaf4
Ubinquitous <ubinquitous1@gmail.com>
2022-09-19 00:30:50 +0900

    UPDATE : 표 형식으로 게시글 스타일 전환

--- src/Components/Router/Management.js
+++ src/Components/Router/Management.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 import Header from '../Header';
 import '../../Style/Management.scss';
 
 const Management = () => {
     const isManager = true;
+    const [testUser, setTestUser] = useState(
+        [
+            {
+                id: '1',
+                text: 'sadnclacnlsacnlskcnlsaasdsklnfkldsvkldvlkdalkfdnalkasdklakdlnclksanclksnakcnsalkcnadlkcnaldkncaslkckadncalkncklascklasnclkanslkcanslkanlvnalkvnaslkvnsalkvnaslknvlkasnvlsanlk',
+                isAnony: true,
+            },
+            {
+                id: '2',
+                text: 'saadsgdsagbdadnclacnlsacnlskcnlsaas',
+                isAnony: false,
+            },
+            {
+                id: '3',
+                text: 'sadnclacnlsacasdvasdbsdabvdavjdabkvjabsjkvbsakjvbaskjvbjkasvn jkasnvkjadnvjkadnkjvadvnlskcnlsaas',
+                isAnony: false,
+            },
+            {
+                id: '4',
+                text: 'sadnclacnlsacnlsk',
+                isAnony: true,
+                image: '/images/picture.png',
+            },
+        ]
+    )
+
+    const onClickTextDetail = (e) => {
+        alert(e.target.value)
+    }
 
     return (
         <div>
@@ -18,33 +47,59 @@ const Management = () => {
                     게시글
                 </h1>
             </div>
-            <div className='content_box_wrap'>
-                <div className='content_box'>
-                    <div className='profile_image_box'>
-                        <img className='profile_image' src='/images/picture.png' alt='프로필' />
+            <div className='management_content_wrap'>
+                <div
+                    className='management_content_box' >
+                    <div className='management_id_box'>
+                        <span className='management_id'>아이디</span>
                     </div>
-                    <div className='user_box'>
-                        <div className='user_name_box'>
-                            <h1 className='user_name'>username</h1>
-                        </div>
-                        <div className='post_date_box'>
-                            <h1 className='post_date'>2022년 9월 31</h1>
-                        </div>
-                    </div>
-                    <div className='management_button_box'>
-                        <button className='accept_button' onClick='#'>수락</button>
-                        <button className='refuse_button' onClick='#'>거절</button>
-                    </div>
-                    <div className='custom_line' />
-                    <div className='contents'>
-                        <h1 className='content'>
-                            just longer letter just longer letter just longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letterjust longer letter
-                        </h1>
-                    </div>
-                    <div className='content_image_box'>
-                        <img className='content_image' src='/images/picture.png' alt='i' />
+                    <div className='management_text_box'>
+                        <span className='management_text'>
+                            게시글
+                        </span>
+                        <button className='text_detail_btn_preview' ></button>
+                        <div className='custome_line' />
+                        <span className='management_isAnony_preview'>익명여부</span>
+                        <div className='custome_line' />
+                        <img src='/images/picture.png' alt='images' className='img_preview' />
+                        <div className='custome_line' />
+                        <button className='accept_btn_preview'>수락 여부</button>
+                        <button className='reject_btn_preview'></button>
+
                     </div>
                 </div>
+
+            </div>
+            <div className='management_content_wrap'>
+                {testUser.map((user) => {
+                    return (
+                        <div key={user.id}
+                            className='management_content_box' >
+                            <div className='management_id_box'>
+                                <span className='management_id'>{user.id}</span>
+                            </div>
+                            <div className='management_text_box'>
+                                <span className='management_text'>{
+                                    user.text.length > 75 ? user.text.slice(0, 75) + ' ...'
+                                        :
+                                        user.text
+                                }
+                                </span>
+                                <button className='text_detail_btn' onClick={onClickTextDetail} value={user.text}>보기</button>
+                                <div className='custome_line' />
+                                <span className='management_isAnony'>{user.isAnony ? '익명' : '비익명'}</span>
+                                <div className='custome_line' />
+                                <img src='/images/picture.png' alt='images' className='img_preview' />
+                                <div className='custome_line' />
+                                <button className='accept_btn'>수락</button>
+                                <div className='custome_line' />
+                                <button className='reject_btn'>거절</button>
+
+                            </div>
+                        </div>
+                    )
+                })
+                }
             </div>
         </div>
     );
