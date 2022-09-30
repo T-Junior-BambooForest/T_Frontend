ff7ee58fda218665b1690b94a00856d39b46d93c
Ubinquitous <ubinquitous1@gmail.com>
2022-09-30 11:48:50 +0900

    UPDATE : 구성 변경, 디자인 변경

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -6,6 +6,7 @@ import Forum from './Forum';
 const Post = ({ userInfo }) => {
     const [date] = useState(new Date());
     const [isAnony, setIsAnony] = useState(true);
+    const [text, setText] = useState('');
     const [postInfo, setPostInfo] = useState([
         {
             postId: 1,
@@ -25,6 +26,7 @@ const Post = ({ userInfo }) => {
     ])
 
     const onClickIsAnony = useCallback(() => {
+        isAnony ? document.getElementById('buttonDOM').style.backgroundColor = '#238636' : document.getElementById('buttonDOM').style.backgroundColor = '#21262D';
         setIsAnony(!isAnony);
     }, [isAnony])
 
@@ -61,33 +63,38 @@ const Post = ({ userInfo }) => {
     }, [isAnony, postInfo, userInfo]
     );
 
-    const onChangeText = useCallback((e) => {
-        setPostInfo({
-            text: e.target.value
-        })
-    }, [])
+    const onChangeText = (e) => {
+        setText(e.target.value)
+    }
 
     return (
-        <div className='article_wrap'>
-            <div className='article_box'>
-                <div className='post_title_box'>
-                    <h1 className='post_title'>글 작성하기</h1>
+        <form onSubmit={onSubmit}>
+            <div className='article_wrap'>
+                <div className='article_box'>
+                    <div className='post_title_box'>
+                        <h1 className='post_title'>제보하기</h1>
+                        <div className='form_boxs'>
+                            <span className='anony_button_span'>익명 여부</span>
+                            <input type='button' className='anony_button' onClick={onClickIsAnony} value='✓' id='buttonDOM' />
+                            <input type='file' className='user__file' />
+                            <button type='submit' id='post' className='post_button' >
+                                <svg width="10" height="10" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M3.33333 17.5C3.39976 17.5081 3.46691 17.5081 3.53333 17.5L6.86667 16.6667C7.01454 16.6316 7.14998 16.5566 7.25833 16.45L17.5 6.17504C17.8104 5.86277 17.9847 5.44035 17.9847 5.00004C17.9847 4.55973 17.8104 4.13731 17.5 3.82504L16.1833 2.50004C16.0285 2.34508 15.8447 2.22215 15.6424 2.13828C15.4401 2.0544 15.2232 2.01123 15.0042 2.01123C14.7851 2.01123 14.5683 2.0544 14.3659 2.13828C14.1636 2.22215 13.9798 2.34508 13.825 2.50004L3.58333 12.7417C3.47566 12.8506 3.39812 12.9855 3.35833 13.1334L2.525 16.4667C2.49505 16.5745 2.48712 16.6873 2.50169 16.7982C2.51626 16.9092 2.55303 17.0161 2.6098 17.1125C2.66658 17.209 2.74219 17.293 2.83214 17.3595C2.92209 17.4261 3.02452 17.4739 3.13333 17.5C3.19976 17.5081 3.26691 17.5081 3.33333 17.5ZM15 3.67504L16.325 5.00004L15 6.32504L13.6833 5.00004L15 3.67504ZM4.925 13.7584L12.5 6.17504L13.825 7.50004L6.24167 15.0834L4.48333 15.5167L4.925 13.7584Z" fill="#E5EDF5" />
+                                </svg>
+                                <span style={{ marginLeft: '4px' }}>게시</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div className='editor_box'>
+                        <textarea className='editor' onChange={onChangeText} value={text} />
+                    </div>
+                    <Forum
+                        postInfo={postInfo}
+                        userInfo={userInfo}
+                    />
                 </div>
-                <div className='editor_box'>
-                    <form onSubmit={onSubmit}>
-                        <input type='text' className='editor' onChange={onChangeText} />
-                        <label htmlFor='anony-button'>익명 여부</label>
-                        <input type='button' id='anony-button' onClick={onClickIsAnony} value='✓' />
-                        <button type='submit'>게시</button>
-                    </form>
-                </div>
-                <Forum
-                    postInfo={postInfo}
-                    userInfo={userInfo}
-                />
             </div>
-        </div>
-
+        </form>
     );
 };
 
