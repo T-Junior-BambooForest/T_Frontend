b97c9fa65ed3e1cfe72eb5198c813f261522c77f
Ubinquitous <ubinquitous1@gmail.com>
2022-10-23 15:18:36 +0900

    TEST : href 삭제

--- src/Components/Router/Login.js
+++ src/Components/Router/Login.js
@@ -8,7 +8,7 @@ const Login = () => {
             <Header />
             <div className='not_found_wrap'>
                 <h1 className='not_found_page'>먼저 로그인을 하셔야합니다!</h1>
-                <a className='home_button' href='https://bssm.kro.kr/oauth/login?clientId=59b9bb6b&redirectURI=http://bsmboo.kro.kr/oauth'>로그인하기</a>
+                <a className='home_button' href=''>로그인하기</a>
             </div>
         </div>
     );
