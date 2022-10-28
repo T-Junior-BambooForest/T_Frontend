64c26e6b69b3fa05de2fd413c46ab0bcfc9d81d0
Ubinquitous <ubinquitous1@gmail.com>
2022-10-28 19:33:10 +0900

    TEST : 로그 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../App';
 import Header from '../Components/Header';
 import Post from '../Components/Post';
 
 const Home = () => {
+    const user = useContext(UserContext);
+    console.log(user)
 
     return (
         <div>
