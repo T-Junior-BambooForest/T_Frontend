ff16f0e78bd82a7576a2422bfc9b48001a48fb37
Ubinquitous <ubinquitous1@gmail.com>
2022-10-29 02:47:35 +0900

    ADD : props 변경

--- src/Components/PostItem.js
+++ src/Components/PostItem.js
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import '../Style/PostItem.scss';
 
-const PostItem = ({ text, id, date }) => {
+const PostItem = ({ num, contents, name, date }) => {
 
     return (
         <div className='content_box_wrap'>
             <div className='content_box'>
                 <div className='header_info_box'>
-                    <span className='count_text'>대나무숲 #{id}번째 제보</span>
+                    <span className='count_text'>대나무숲 #{num}번째 제보</span>
                     <div className='date_text_box'>
-                        <span className='date_text'>{'date'}</span>
+                        <span className='date_text'>{date}</span>
                     </div>
                     <div className='author_text_box'>
-                        <span className='author_text'>{'박우빈'}</span>
+                        <span className='author_text'>{name}</span>
                     </div>
                 </div>
                 <div className='text_box'>
-                    <span className='content_text'>{text}</span>
+                    <span className='content_text'>{contents}</span>
                 </div>
             </div>
         </div >
