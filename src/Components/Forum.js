1e9a9d70fdef0342b8e1f9c6e22d1e6a5269c05e
Ubinquitous <ubinquitous1@gmail.com>
2022-10-29 03:16:15 +0900

    TEST : 완료

--- src/Components/Forum.js
+++ src/Components/Forum.js
@@ -42,7 +42,7 @@ const Forum = () => {
                 {allowPost && allowPost.map((post, index) => (
                     <PostItem
                         key={post.boardCode}
-                        num={parseInt(index.length) - parseInt(index)}
+                        num={parseInt(post.length) - parseInt(index)}
                         contents={post.contents}
                         name={post.User.name}
                         date={post.createdAt}
