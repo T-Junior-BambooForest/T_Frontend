217f3528909e5fe2068c44fc9e65e454ac585409
Ubinquitous <ubinquitous1@gmail.com>
2022-10-29 14:29:02 +0900

    ADD : env

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,7 +8,7 @@ const Login = () => {
             <Header />
             <div className='not_found_wrap'>
                 <h1 className='not_found_page'>먼저 로그인을 하셔야합니다!</h1>
-                <a className='home_button' href='https://auth.bssm.kro.kr/oauth?clientId=4bff670f&redirectURI=http://bsmboo.kro.kr:8000/oauth'>로그인하기</a>
+                <a className='home_button' href={process.env.REACT_APP_LOGIN_URL}>로그인하기</a>
             </div>
         </div>
     );
