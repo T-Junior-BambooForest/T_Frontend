6f794531f49f6b25c4433ac38d335c96e303d949
Ubinquitous <ubinquitous1@gmail.com>
2022-10-29 17:46:39 +0900

    FIX : onerror

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -27,7 +27,7 @@ const MyPage = () => {
                     <div className='myprofile_image_box'>
                         <img className='myprofile_image'
                             src={`https://auth.bssm.kro.kr/_next/image?url=https%3A%2F%2Fauth.bssm.kro.kr%2Fresource%2Fuser%2Fprofile%2F${user.code}.png&w=256&q=75`}
-                            onError="this.src='https://bssm.kro.kr/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fprofile_default.99e93808.png&w=128&q=75" alt='profile' />
+                            onError={"https://bssm.kro.kr/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fprofile_default.99e93808.png&w=128&q=75"} alt={'profile'} />
                     </div>
                     <div className='user_info_wrap' style={{ marginTop: '30px' }}>
                         <div className='user_nickname_box'>
