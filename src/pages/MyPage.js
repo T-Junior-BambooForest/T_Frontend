5e3eae34412cff84533c3712133247009401c328
Ubinquitous <ubinquitous1@gmail.com>
2022-10-29 18:04:33 +0900

    ADD : funcion으로 onerror 변경

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -8,6 +8,10 @@ const MyPage = () => {
     const user = useContext(UserContext);
     const navigate = useNavigate();
 
+    const onDefaultProfile = (e) => {
+        e.target.src = "https://bssm.kro.kr/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fprofile_default.99e93808.png&w=128&q=75";
+    }
+
     useEffect(() => {
         if (!user.isLogin) {
             navigate('/login')
@@ -27,7 +31,7 @@ const MyPage = () => {
                     <div className='myprofile_image_box'>
                         <img className='myprofile_image'
                             src={`https://auth.bssm.kro.kr/_next/image?url=https%3A%2F%2Fauth.bssm.kro.kr%2Fresource%2Fuser%2Fprofile%2F${user.code}.png&w=256&q=75`}
-                            onError={"https://bssm.kro.kr/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fprofile_default.99e93808.png&w=256&q=75"} alt={'profile'} />
+                            onError={onDefaultProfile} alt={'profile'} />
                     </div>
                     <div className='user_info_wrap' style={{ marginTop: '30px' }}>
                         <div className='user_nickname_box'>
