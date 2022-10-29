10dc633a086bf9a2ea9d5d4803c945dfbe42f53a
Ubinquitous <ubinquitous1@gmail.com>
2022-10-29 14:44:33 +0900

    UPDATE : 보안

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,12 +3,17 @@ import '../Style/NotFound.scss';
 import Header from '../Components/Header';
 
 const Login = () => {
+    function validateURL(url) {
+        const parsed = new URL(url)
+        return ['https:', 'http:'].includes(parsed.protocol)
+    }
+
     return (
         <div>
             <Header />
             <div className='not_found_wrap'>
                 <h1 className='not_found_page'>먼저 로그인을 하셔야합니다!</h1>
-                <a className='home_button' href={process.env.REACT_APP_LOGIN_URL}>로그인하기</a>
+                <a href={validateURL(process.env.REACT_APP_LOGIN_URL) ? process.env.REACT_APP_LOGIN_URL : ''}>로그인하기</a>
             </div>
         </div>
     );
