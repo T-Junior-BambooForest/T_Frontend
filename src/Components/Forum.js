e682b688447313479804aef0d4f24685b17f7451
Ubinquitous <ubinquitous1@gmail.com>
2022-11-06 22:13:10 +0900

    FIX

--- src/Components/Forum.js
+++ src/Components/Forum.js
@@ -10,7 +10,7 @@ const Forum = () => {
         (async () => {
             try {
                 let data = await getAllowPostInfo();
-                setAllowPost(...data.data)
+                setAllowPost(data.data.data)
             } catch (error) {
                 if (error instanceof AxiosError && error.response?.status >= 400) {
                     setAllowPost((prev) => ({ ...prev, isLogin: false }));
