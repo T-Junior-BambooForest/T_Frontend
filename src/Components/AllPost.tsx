9656e44d145e3570707a9a14532d58bd808b09bb
Ubinquitous <ubinquitous1@gmail.com>
2022-11-10 10:00:49 +0900

    UPDATE : 코드 리팩토링, 타입설정, TS로 변환, baseURL 설정

---
+++ src/Components/AllPost.tsx
@@ -0,0 +1,63 @@
+import axios, { AxiosError } from 'axios';
+import React, { useEffect, useState } from 'react';
+import '../Style/Forum.scss';
+import PostItem from './PostItem';
+
+type AllowBoard = {
+    AllowBoardCode: number,
+}
+
+type UserType = {
+    name: string,
+}
+
+interface PostType {
+    AllowBoard: AllowBoard,
+    contents: string,
+    User: UserType,
+    createdAt: any
+}
+
+const Forum = () => {
+    const [allowPost, setAllowPost]: any = useState();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const post = await getAllowPostInfo();
+                setAllowPost(post.data)
+            } catch (error) {
+                if (error instanceof AxiosError && error.response?.data?.code >= 400) {
+                    console.log(error)
+                }
+            }
+        })();
+    }, []);
+
+    const getAllowPostInfo = () => {
+        return axios.get('/board', { withCredentials: true });
+    };
+
+    return (
+        <div className='forum_wrap'>
+            <div className='article_title_box'>
+                <h1 className='article_title' style={localStorage.getItem('theme') === 'dark' ? { color: 'white' } : { color: 'black' }}>
+                    모든 제보
+                </h1>
+            </div>
+            <div>
+                {allowPost && allowPost?.map((post: PostType) => (
+                    <PostItem
+                        key={post.AllowBoard.AllowBoardCode}
+                        num={post.AllowBoard.AllowBoardCode}
+                        contents={post.contents}
+                        name={post.User.name}
+                        date={post.createdAt}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Forum;
