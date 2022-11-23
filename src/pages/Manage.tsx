247c8d6e1be1186516f3b31a08cf94a312d2fb9d
Ubinquitous <ubinquitous1@gmail.com>
2022-11-23 16:18:08 +0900

    FIX : 경로 수정

--- src/pages/Manage.tsx
+++ src/pages/Manage.tsx
@@ -56,7 +56,7 @@ const Management = () => {
                 const data = await getPostInfo();
                 setPost(data.data)
                 console.log(data.data)
-                console.log(data.data.toString('base64'))
+                console.log(data.data[8].Image.toString('base64'))
                 setIsLoad(true)
             } catch (error) {
                 if (error instanceof AxiosError && error.response?.status >= 400) {
