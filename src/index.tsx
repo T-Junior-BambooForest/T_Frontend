d7b63941e8714dc922d99978ca16ce1cbaf1c30e
Ubinquitous <ubinquitous1@gmail.com>
2023-01-21 00:00:07 +0900

    ADD : redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { legacy_createStore as createStore } from 'redux'
+import reducer from './modules'
+import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(reducer)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <App />
-);
+	<Provider store={store}>
+		<App />
+	</Provider>
+)
