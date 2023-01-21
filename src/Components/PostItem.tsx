79207c3e9b9f84edc07fdc8c4135219fecf7c81a
Ubinquitous <ubinquitous1@gmail.com>
2023-01-22 03:49:23 +0900

    UPDATE : UI 변경

--- src/Components/PostItem.tsx
+++ src/Components/PostItem.tsx
@@ -21,25 +21,18 @@ const PostItem = ({ category, isAnonymous, contents, allowCode, image, user }: P
 		<div className="content_box_wrap">
 			<div className="content_box">
 				<div className="header_info_box">
-					<span className="count_text">대나무숲 #{allowCode}번째 제보</span>
+					<span className="count_text">부마숲 {allowCode}번째 이야기</span>
 					<div className="date_text_box">
+						|<span className="category_text">#{category}</span>
 						<span className="date_text">
-							{/* {parseInt(date.slice(11, 13)) > 12
-								? `${date.slice(0, 4)}년 ${date.slice(5, 7)}월 ${date.slice(8, 10)}일 PM ${
-										parseInt(date.slice(11, 13)) - 12
-								  }${date.slice(13, 16)}`
-								: `${date.slice(0, 4)}년 ${date.slice(5, 7)}월 ${date.slice(8, 10)}일 AM ${parseInt(
-										date.slice(11, 13)
-								  )}${date.slice(13, 16)}`} */}
+							{isAnonymous ? '익명' : `${user.grade}학년 ${user.class}반 ${user.name}`}님 제보
 						</span>
-						<span>{category}</span>
 					</div>
 					<div className="author_text_box">
-						<span className="author_text">
-							{isAnonymous ? '익명' : `${user.grade}학년 ${user.class}반 ${user.name}`}
-						</span>
+						<span className="author_text">2023년 1월 22일 일요일 4시 32분</span>
 					</div>
 				</div>
+				<div className="content_lines"></div>
 				<div className="text_box">
 					<span className="content_text">{contents.replace(/<br>/gi, '\n')}</span>
 					{image ? <img src={image} alt="img" className="img" /> : ''}
