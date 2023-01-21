07d22eaa78c9120352efc3817da3e25de0aa8f03
Ubinquitous <ubinquitous1@gmail.com>
2023-01-22 02:48:24 +0900

    UPDATE : 백엔드 AllowPost 형식 변경

--- src/Components/AllPost.tsx
+++ src/Components/AllPost.tsx
@@ -4,61 +4,73 @@ import '../Style/Forum.scss'
 import PostItem from './PostItem'
 
 interface AllowPostType {
-    AllowBoard: AllowBoard
-    contents: string,
-    User: User,
-    createdAt: string,
-    Image: string,
+	AllowedCode: number
+	post: {
+		postCode: number
+		category: string
+		isAnonymous: boolean
+		contents: string
+		Image: string
+		user: {
+			code: number
+			role: string
+			grade: number
+			class: number
+			name: string
+		}
+	}
 }
 
 type User = {
-    name: string,
+	name: string
 }
 
 type AllowBoard = {
-    AllowBoardCode: number
+	AllowBoardCode: number
 }
 
 const AllPost = () => {
-    const [allowPost, setAllowPost] = useState([])
+	const [allowPost, setAllowPost] = useState([])
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const post = await getAllowPostInfo()
-                setAllowPost(post.data)
-            } catch (error) {
-                if (error instanceof AxiosError && error.response?.data?.code >= 400) {
-                    alert('에러가 발생했습니다.')
-                    console.log(error)
-                }
-            }
-        })()
-    }, [])
+	useEffect(() => {
+		;(async () => {
+			try {
+				const res = await getAllowPostInfo()
+				setAllowPost(res.data)
+			} catch (error) {
+				if (error instanceof AxiosError && error.response?.data?.code >= 400) {
+					alert('에러가 발생했습니다.')
+					console.log(error)
+				}
+			}
+		})()
+	}, [])
 
-    const getAllowPostInfo = () => {
-        return axios.get('/board', { withCredentials: true })
-    };
+	const getAllowPostInfo = () => {
+		return axios.get('/board', { withCredentials: true })
+	}
 
-    return (
-        <div className='forum_wrap'>
-            <div className='article_title_box'>
-                <h1 className='article_title'>모든 제보</h1>
-            </div>
-            <div>
-                {allowPost && allowPost?.map((post: AllowPostType) => (
-                    <PostItem
-                        key={post.AllowBoard.AllowBoardCode}
-                        num={post.AllowBoard.AllowBoardCode}
-                        contents={post.contents}
-                        name={post.User.name}
-                        date={post.createdAt}
-                        blobImg={post?.Image}
-                    />
-                ))}
-            </div>
-        </div>
-    );
-};
+	return (
+		<div className="forum_wrap">
+			<div className="article_title_box">
+				<h1 className="article_title">모든 제보</h1>
+			</div>
+			<div>
+				{allowPost &&
+					allowPost?.map((post: AllowPostType) => (
+						<PostItem
+							key={post.post.postCode}
+							category={post.post.category}
+							isAnonymous={post.post.isAnonymous}
+							contents={post.post.contents}
+							allowCode={post.AllowedCode}
+							image={post.post.Image}
+							user={post.post.user}
+						/>
+					))}
+			</div>
+		</div>
+	)
+}
 
-export default AllPost;
+export default AllPost
