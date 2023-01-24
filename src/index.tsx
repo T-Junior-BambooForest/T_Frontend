bd7c8755e663677429f5ebc934b57de233b2481b
Ubinquitous <ubinquitous1@gmail.com>
2023-01-24 15:38:12 +0900

    ADD : react-query setting

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,32 @@ import App from './App'
 import { legacy_createStore as createStore } from 'redux'
 import reducer from './modules'
 import { Provider } from 'react-redux'
+import { QueryCache, QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 const store = createStore(reducer)
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			retry: 0,
+			suspense: false,
+		},
+	},
+	queryCache: new QueryCache({
+		onError: (err) => {
+			console.log(err)
+		},
+	}),
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<Provider store={store}>
-		<App />
-	</Provider>
+	<QueryClientProvider client={queryClient}>
+		<ReactQueryDevtools initialIsOpen={true} />
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</QueryClientProvider>
 )
